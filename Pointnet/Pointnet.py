Fixed normalization bug and simplified Model

Pointnet/Pointnet.py
@@ -54,8 +54,8 @@ class Pointnet_Encoder(nn.Module):
     def __init__(self, dim=3, shared_A=(64, 64), shared_B=(64, 128, 1024)):
         super(Pointnet_Encoder, self).__init__()
         # transform Networks
-        self.tnet_A = TNet(dim=dim)
-        self.tnet_B = TNet(dim=shared_A[-1])
+        self.tnet_A = TNet(dim=dim, encoder=(64,), decoder=(32,))
+        self.tnet_B = TNet(dim=shared_A[-1], encoder=(64,), decoder=(32,))
         # shared mlps
         self.convs_A = nn.ModuleList([nn.Conv1d(in_, out_, 1) for in_, out_ in zip((dim,) + shared_A[:-1], shared_A)])
         self.convs_B = nn.ModuleList([nn.Conv1d(in_, out_, 1) for in_, out_ in zip((shared_A[-1],) + shared_B[:-1], shared_B)])
@@ -91,7 +91,9 @@ class Pointnet_Classification(nn.Module):
         # dropout-layer
         self.dropout = nn.Dropout(dropout_rate)
 
-    def forward(self, x):
+    def forward(self, global_local_feats):
+        # get features of interest
+        x = global_local_feats[0]
         # pass theough linear layers
         for linear, bn in zip(self.linear, self.batchnorms):
             x = torch.sigmoid(bn(linear(x)))
@@ -113,8 +115,9 @@ class Pointnet_Segmentation(nn.Module):
         # create classification layer
         self.classify = nn.Conv1d(shared[-1], k, 1)
 
-    def forward(self, global_feats, local_feats):
-
+    def forward(self, global_local_feats):
+        # separate features
+        global_feats, local_feats = global_local_feats
         # pretend concatenation of global and local features
         y = self.convs[0](local_feats) + self.linear(global_feats).unsqueeze(2)
         y = F.relu(self.batchnorms[0](y))

models.py
@@ -6,11 +6,104 @@ from Pointnet.PointnetPP import PointnetPP_Encoder, PointnetPP_Classification, P
 # import others
 import os
 
+# improt pytorch-framework
+import torch
+import torch.nn as nn
+# import PointnetPP
+from Pointnet.Pointnet import Pointnet_Encoder, Pointnet_Classification, Pointnet_Segmentation
+from Pointnet.PointnetPP import PointnetPP_Encoder, PointnetPP_Classification, PointnetPP_Segmentation
+# import others
+import os
+
+# *** POINTNET ***
+
 class Model_CLS(nn.Module):
+    """ Pointnet for Classification """
+
+    def __init__(self, K, feat_dim=3):
+        super(Model_CLS, self).__init__()
+        # encoder and classifier
+        self.encoder = Pointnet_Encoder(dim=3+feat_dim, shared_A=(32,), shared_B=(64,))
+        self.classifier = Pointnet_Classification(k=K, g=64, shape=(128,))
+        # creterion
+        self.criterion = nn.NLLLoss()
+
+    def forward(self, x):
+        # encode and classify
+        return self.classifier(self.encoder(x))
+
+    def loss(self, y, y_hat):
+        # compute loss
+        return self.criterion(y, y_hat)
+
+    def save(self, file_path):
+        # save encoder and classifier separatly
+        torch.save(self.encoder.state_dict(), os.path.join(file_path, "encoder.model"))
+        torch.save(self.classifier.state_dict(), os.path.join(file_path, "classifier.model"))
+
+    def load_encoder(self, file_path):
+        # check if a file is given
+        if file_path is None:
+            return
+        # load encoder state dict
+        self.encoder.load_state_dict(torch.load(file_path))
+
+    def load_classifier(self, file_path):
+        # check if a file is given
+        if file_path is None:
+            return
+        # load encoder state dict
+        self.classifier.load_state_dict(torch.load(file_path))
+
+class Model_SEG(nn.Module):
+    """ Pointnet for Segmentation """
+
+    def __init__(self, K, feat_dim=3):
+        super(Model_SEG, self).__init__()
+        # save number of classes
+        self.K = K
+        # encoder and segmentater
+        self.encoder = Pointnet_Encoder(dim=3+feat_dim, shared_A=(32,), shared_B=(64,))
+        self.segmentater = Pointnet_Segmentation(k=K, g=64, l=32, shared=(128,))
+        # creterion
+        self.criterion = nn.NLLLoss()
+
+    def forward(self, x):
+        # encode and classify
+        return self.segmentater(self.encoder(x))
+
+    def loss(self, y, y_hat):
+        # compute loss
+        return self.criterion(y.reshape(-1, self.K), y_hat.flatten())
+
+    def save(self, file_path):
+        # save encoder and segmentater separatly
+        torch.save(self.encoder.state_dict(), os.path.join(file_path, "encoder.model"))
+        torch.save(self.segmentater.state_dict(), os.path.join(file_path, "segmentater.model"))
+
+    def load_encoder(self, file_path):
+        # check if a file is given
+        if file_path is None:
+            return
+        # load encoder state dict
+        self.encoder.load_state_dict(torch.load(file_path, map_location='cpu'))
+
+    def load_segmentater(self, file_path):
+        # check if a file is given
+        if file_path is None:
+            return
+        # load encoder state dict
+        self.segmentater.load_state_dict(torch.load(file_path, map_location='cpu'))
+
+
+
+# *** POINTNET++ ***
+
+class ModelPP_CLS(nn.Module):
     """ Pointnet++ for Classification """
 
     def __init__(self, K):
-        super(Model_CLS, self).__init__()
+        super(ModelPP_CLS, self).__init__()
         # encoder and classifier
         self.encoder = PointnetPP_Encoder(pos_dim=3, feat_dim=3)
         self.classifier = PointnetPP_Classification(k=K)
@@ -44,12 +137,11 @@ class Model_CLS(nn.Module):
         # load encoder state dict
         self.classifier.load_state_dict(torch.load(file_path))
 
-
-class Model_SEG(nn.Module):
+class ModelPP_SEG(nn.Module):
     """ Pointnet++ for Segmentation """
 
     def __init__(self, K, feat_dim=3):
-        super(Model_SEG, self).__init__()
+        super(ModelPP_SEG, self).__init__()
         # save number of classes
         self.K = K
         # encoder and segmentater
@@ -84,5 +176,4 @@ class Model_SEG(nn.Module):
             return
         # load encoder state dict
         self.segmentater.load_state_dict(torch.load(file_path, map_location='cpu'))
-
 

train_cls.py
@@ -17,34 +17,41 @@ import os
 from tqdm import tqdm
 from random import sample
 from collections import OrderedDict
+import matplotlib.pyplot as plt
 
 # numpy percision for printing
 np.set_printoptions(precision=3)
 
+
 # *** PARAMS ***
 
 # cuda device
-device = 'cuda:0'
+device = 'cpu'
 # number of classes
 classes = ['CB', 'D', 'PN', 'R']
 K = len(classes)
 # path to files
-fpath = "C:/Users/doll0.SGN/Documents/Grapes/BBCH87_89/"
+fpath = "C:/Users/Niclas/Documents/Pointclouds/Bunch"
 # number of points to use in training
-n_points = 20_000
+n_points = 1024
 # number of samples per pointcloud
-n_samples = 10
+n_samples = 500
+# number of poinclouds per class for testing
+samples_for_testing = 5
+# initial checkpoint
+encoder_init_checkpoint = None
+classifier_init_checkpoint = None
 # save path
-save_path = "C:/Users/doll0.SGN/Documents/results/test"
+save_path = "results/classify"
 os.makedirs(save_path, exist_ok=True)
 
 # training parameters
-epochs = 500
-batch_size = 6
+epochs = 100
+batch_size = 20
 # update parameters after n batches
 update_interval = 1
 # save model after every n-th epoch
-save_interval = 2
+save_interval = 1
 
 
 # *** READ AND CREATE TRAINING / TESTING DATA ***
@@ -52,28 +59,67 @@ save_interval = 2
 pointclouds = OrderedDict()
 # open files
 for directory in tqdm(os.listdir(fpath)):
-    # create entry
+    # ignore processed
+    if directory == 'Processed':
+        continue
+    # build full directory
     full_dir = os.path.join(fpath, directory)
-    pointclouds[directory] = [np.loadtxt(os.path.join(full_dir, fname), dtype=np.float32) for fname in os.listdir(full_dir)]
+    # only read original files in
+    pointclouds[directory] = [np.loadtxt(os.path.join(full_dir, fname), dtype=np.float32) for fname in os.listdir(full_dir) if 'OR' in fname]
 
 # build data
-x_train, y_train, x_test, y_test = build_data_cls(pointclouds, n_points, n_samples)
+x_train, y_train, x_test, y_test = build_data_cls(pointclouds, n_points, n_samples, features=['points', 'colors'])
 
 
 # *** CREATE MODEL AND OPTIMIZER ***
 
 # create model
-model = Model_CLS(K=K).to(device)
+model = Model_CLS(K=K, feat_dim=x_train.size(1) - 3)
+model.load_encoder(encoder_init_checkpoint)
+model.load_classifier(classifier_init_checkpoint)
+model.to(device)
 # create optimizer
 optim = optimizer.Adam(model.parameters())
 
 
 # *** TRAIN AND TEST MODEL ***
 
+# track losses
+losses = []
+# callback function
+def callback(epoch, loss):
+    # add loss to losses
+    losses.append(loss)
+    # save
+    if epoch % save_interval == 0:
+
+        # build confusion table
+        test_confusion = create_confusion_matrix(model, x_test, y_test, len(classes), batch_size, device=device)
+        train_confusion = create_confusion_matrix(model, x_train, y_train, len(classes), batch_size, device=device)
+        try:
+            # save confusion matrix
+            visualize_confusion_table(test_confusion, classes=classes, normalize=True)[0].savefig(os.path.join(save_path, "confusion-test.pdf"), format='pdf')
+            visualize_confusion_table(train_confusion, classes=classes, normalize=True)[0].savefig(os.path.join(save_path, "confusion-train.pdf"), format='pdf')
+        except Exception as e:
+            # handle exception
+            print("[EXCEPTION]", e)
+
+        # create loss graph
+        fig, ax = plt.subplots(1, 1)
+        ax.set_xlabel("Epochs")
+        ax.set_ylabel("Loss")
+        # plot losses and save figure
+        ax.plot(losses)
+        try:
+            # save loss-graph
+            fig.savefig(os.path.join(save_path, "lossgraph.pdf"), format='pdf')
+        except Exception as e:
+            print("[EXCEPTION] during save of plot:", e)
+
+        # close figure to free memory
+        plt.close()
+        # save model
+        model.save(save_path)
+
 # train
-train_model(model, x_train, y_train, optim, epochs, batch_size, update_interval, save_interval, save_path, device=device)
-# build confusion table
-confusion = create_confusion_matrix(model, x_test, y_test, K, batch_size, device=device)
-# save confusion matrix
-visualize_confusion_table(confusion, classes=classes, normalize=True)[0].savefig(os.path.join(save_path, "confusion-norm.pdf"), format='pdf')
-visualize_confusion_table(confusion, classes=classes, normalize=False)[0].savefig(os.path.join(save_path, "confusion-total.pdf"), format='pdf')
+train_model(model, x_train, y_train, optim, epochs, batch_size, update_interval, device=device, callback=callback)

train_seg.py
@@ -16,6 +16,7 @@ from utils import normalize_pc, visualize_confusion_table, create_confusion_matr
 import os
 from tqdm import tqdm
 from random import sample, shuffle
+import matplotlib.pyplot as plt
 
 # numpy percision for printing
 np.set_printoptions(precision=3)
@@ -24,58 +25,57 @@ np.set_printoptions(precision=3)
 # *** PARAMS ***
 
 # cuda device
-device = 'cuda:0'
+device = 'cpu'
 # number of classes
 K = 7
+classes = list(range(1, K+1))
 # path to files
-fpath = "C:/Users/doll0.SGN/Documents/Grapes/Skeletons_seg_normals/"
+fpath = "C:/Users/Niclas/Documents/Pointclouds/Skeleton/Processed"
 # number of points to use in training
-n_points = 25_000
+n_points = 1024
 # number of samples per pointcloud
-n_samples = 10
+n_samples = 3000
 # number of poinclouds per class for testing
 samples_for_testing = 1
 # initial checkpoint
 encoder_init_checkpoint = None
 segmentater_init_checkpoint = None
 # save path
-save_path = "C:/Users/doll0.SGN/Documents/results/Skeletons_normals"
+save_path = "results/segmentate"
 os.makedirs(save_path, exist_ok=True)
 
 # training parameters
-epochs = 5000
-batch_size = 3
+epochs = 100
+batch_size = 10
 # update parameters after n batches
-update_interval = 2
+update_interval = 1
 # save model after every n-th epoch
-save_interval = 2
+save_interval = 1
 
 
 # *** READ AND CREATE TRAINING / TESTING DATA ***
 
 pointclouds = {}
 # open files
-for directory in tqdm(os.listdir(fpath)):
-    # create entry
-    full_dir = os.path.join(fpath, directory)
-    # pointclouds[directory] = np.random.uniform(-1, 1, size=(len(os.listdir(full_dir)), 2 * n_points, 6)) * 100
-    pointclouds[directory] = [np.loadtxt(os.path.join(full_dir, fname), dtype=np.float32) for fname in os.listdir(full_dir)]
-
+for fname in tqdm(os.listdir(fpath)):
+    # get class of pointcloud
+    class_name = fname.split('_')[0]
+    # check for entry in pointclouds
+    if class_name not in pointclouds:
+        pointclouds[class_name] = []
+    # create full path to file
+    full_path = os.path.join(fpath, fname)
+    # read pointcloud
+    pointclouds[class_name].append(np.loadtxt(full_path, dtype=np.float32))
+    
 # build data from pointclouds
-x_train, y_train, x_test, y_test = build_data_seg(pointclouds, n_points, n_samples, samples_for_testing)
-# get feature-dimension
-feat_dim = x_train.size(1) - 3
+x_train, y_train, x_test, y_test = build_data_seg(pointclouds, n_points, n_samples, samples_for_testing, features=['points', 'colors', 'length'])
 
-
-# *** ANALYSE DATA ***
-
-# print(list([np.sum(y_train.numpy()==i) for i in np.unique(y_train)]))
-# exit()
 
 # *** CREATE MODEL AND OPTIMIZER ***
 
 # create model
-model = Model_SEG(K=K, feat_dim=feat_dim)
+model = Model_SEG(K=K, feat_dim=x_train.size(1) - 3)
 model.load_encoder(encoder_init_checkpoint)
 model.load_segmentater(segmentater_init_checkpoint)
 model.to(device)
@@ -85,10 +85,42 @@ optim = optimizer.Adam(model.parameters())
 
 # *** TRAIN AND TEST MODEL ***
 
+# track losses
+losses = []
+# callback function
+def callback(epoch, loss):
+    # add loss to losses
+    losses.append(loss)
+    # save
+    if epoch % save_interval == 0:
+
+        # build confusion table
+        test_confusion = create_confusion_matrix(model, x_test, y_test, len(classes), batch_size, device=device)
+        train_confusion = create_confusion_matrix(model, x_train, y_train, len(classes), batch_size, device=device)
+        try:
+            # save confusion matrix
+            visualize_confusion_table(test_confusion, classes=classes, normalize=True)[0].savefig(os.path.join(save_path, "confusion-test.pdf"), format='pdf')
+            visualize_confusion_table(train_confusion, classes=classes, normalize=True)[0].savefig(os.path.join(save_path, "confusion-train.pdf"), format='pdf')
+        except Exception as e:
+            # handle exception
+            print("[EXCEPTION]", e)
+
+        # create loss graph
+        fig, ax = plt.subplots(1, 1)
+        ax.set_xlabel("Epochs")
+        ax.set_ylabel("Loss")
+        # plot losses and save figure
+        ax.plot(losses)
+        try:
+            # save loss-graph
+            fig.savefig(os.path.join(save_path, "lossgraph.pdf"), format='pdf')
+        except Exception as e:
+            print("[EXCEPTION] during save of plot:", e)
+
+        # close figure to free memory
+        plt.close()
+        # save model
+        model.save(save_path)
+
 # train
-train_model(model, x_train, y_train, optim, epochs, batch_size, update_interval, save_interval, save_path, device=device)
-# build confusion table
-confusion = create_confusion_matrix(model, x_test, y_test, K, batch_size, device=device)
-# save confusion matrix
-visualize_confusion_table(confusion, classes=range(1, K+1), normalize=True)[0].savefig(os.path.join(save_path, "confusion-norm.pdf"), format='pdf')
-visualize_confusion_table(confusion, classes=range(1, K+1), normalize=False)[0].savefig(os.path.join(save_path, "confusion-total.pdf"), format='pdf')
+train_model(model, x_train, y_train, optim, epochs, batch_size, update_interval, device=device, callback=callback)

utils.py
@@ -15,11 +15,11 @@ from time import time
 
 """ Pointcloud Helpers """
 
-def normalize_pc(pc, axis=1):
+def normalize_pc(pc, reduce_axis=0, feature_axis=1):
 
     # transform relative to centroid and resclae
-    pc = pc - np.mean(pc, axis=0, keepdims=True)
-    pc = pc / np.max(np.linalg.norm(pc, axis=axis, keepdims=True))
+    pc = pc - np.mean(pc, axis=reduce_axis, keepdims=True)
+    pc = pc / np.max(np.linalg.norm(pc, axis=feature_axis, keepdims=True))
     # return 
     return pc
 
@@ -50,11 +50,10 @@ def estimate_normals(points):
 
 """ Train Helpers """
 
-def train_model(model, x_train, y_train, optim, epochs, batch_size, update_interval, save_interval, save_path, device='cpu'):
+def train_model(model, x_train, y_train, optim, epochs, batch_size, update_interval, device='cpu', callback=lambda:None):
     # set model mode
     model.train()
     # train model
-    losses = []
     start_time = time()
     train_size = x_train.shape[0]
     for e in range(1, 1+epochs):
@@ -78,40 +77,20 @@ def train_model(model, x_train, y_train, optim, epochs, batch_size, update_inter
             running_loss += loss.item()
             # optimization
             if (b > 0 and b % update_interval == 0) or (b == train_size//batch_size - 1):
-                print("Epoch: {0} -\tBatch: {1} -\tLoss: {2:.04f} -\tBatch Time: {3:.02f} -\tTotal Time: {4:.02f}".format(e, b, loss.item(), time() - batch_start_time, time() - start_time))
                 # update model parameters
                 optim.zero_grad()
                 loss.backward()
                 optim.step()
+                # log
+                print("Epoch: {0} -\tBatch: {1} -\tLoss: {2:.04f} -\tBatch Time: {3:.02f} -\tTotal Time: {4:.02f}".format(e, b, loss.item(), time() - batch_start_time, time() - start_time))
                 # reset for next iteration
                 batch_start_time = time()
                 loss = 0
             # remove - free gpu memory
             del x_batch, y_batch
         
-        # add loss to losses
-        losses.append(running_loss)
-        # save
-        if e % save_interval == 0:
-            # save model after enough number of epochs
-            model.save(save_path)
-            # create loss graph
-            fig, ax = plt.subplots(1, 1)
-            ax.set_xlabel("Epochs")
-            ax.set_ylabel("Loss")
-            # plot losses and save figure
-            ax.plot(losses)
-            try:
-                fig.savefig(os.path.join(save_path, "lossgraph.pdf"), format='pdf')
-            except Exception as e:
-                print("[EXCEPTION] during save of plot:", e)
-            # close figure to free memory
-            plt.close()
-
-    # save final encoder and classifier
-    model.save(save_path)
-    # return losses
-    return losses
+        # callback
+        callback(0 if e == epochs else e, running_loss)
 
 
 """ Evaluation Helpers """

visualize.py
@@ -58,56 +58,47 @@ class Visualizer:
 
 if __name__ == '__main__':
     # import colors of classes
-    from data import color2class
+    from data import color2class, build_data_seg
     # import segmentation model
     from models import Model_SEG
     # import torch
     import torch
 
     # *** SET UP ***
-    example_fpath = "C:/Users/doll0.SGN/Documents/Grapes/Skeletons_seg_normals/CalardisBlanc/1E.xyzrgbc"
-    encoder_fpath = "I:/Skeletons_normals_equally_distributed/encoder.model"
-    segmentater_fpath = "I:/Skeletons_normals_equally_distributed/segmentater.model"
+    example_fpath = "C:/Users/Niclas/Documents/Pointclouds/Skeleton/Processed/PinotNoir_1.xyzrgbc"
+    encoder_fpath = "results/segmentate/encoder.model"
+    segmentater_fpath = "results/segmentate/segmentater.model"
 
-    # load file to numpy array
+    # *** PARAMETERS ***
+
+    n_points = 10240
+    n_samples = 5
+
+    # get pointcloud features
     pc_raw = np.loadtxt(example_fpath)
-    # separate array
-    points = pc_raw[:, 0:3]
-    colors = pc_raw[:, 3:6] / 255
-    classes = pc_raw[:, -1:]
-    # create color from classes
+    points, colors, classes = normalize_pc(pc_raw[:, 0:3]), pc_raw[:, 3:6] / 255, pc_raw[:, -1:]
+    # get ground truth colors
     class_colors = np.apply_along_axis(lambda i: color2class[int(i)], 1, classes) / 255
-    # create original pointcloud
-    pc_original = open3d.geometry.PointCloud()
-    pc_original.points = open3d.utility.Vector3dVector(points)
-    pc_original.colors = open3d.utility.Vector3dVector(colors)
-    # create ground truth poitncloud
-    pc_ground_truth = open3d.geometry.PointCloud()
-    pc_ground_truth.points = open3d.utility.Vector3dVector(points)
-    pc_ground_truth.colors = open3d.utility.Vector3dVector(class_colors)
 
     # create and load model
-    model = Model_SEG(K=len(color2class), feat_dim=6)
+    model = Model_SEG(K=7, feat_dim=4)
     model.load_encoder(encoder_fpath)
     model.load_segmentater(segmentater_fpath)
     model.eval()
-    # predict classes of points
-    x = torch.from_numpy(pc_raw[:, :-1]).float().t().unsqueeze(0)
-    log_probs = model.forward(x)
-    predicted = torch.max(log_probs, dim=2)[1]
-    predicted = predicted.cpu().numpy()
-    # get predicted colors
-    predicted_colors = np.apply_along_axis(lambda i: color2class[int(i)], 1, predicted.T) / 255
-    # build pointcloud
-    pc_predicted = open3d.geometry.PointCloud()
-    pc_predicted.points = open3d.utility.Vector3dVector(points)
-    pc_predicted.colors = open3d.utility.Vector3dVector(predicted_colors)
 
+    # predict classes of points
+    x, _, _, _ = build_data_seg({'A': [pc_raw]}, n_points, n_samples, 0, features=['points', 'colors', 'length'])  
+    log_probs = model.forward(x)
+    predicted = torch.max(log_probs, dim=2)[1].view(-1, 1).cpu().numpy()
+    # get processed points and predicted colors
+    processed_points = x[:, :3, :].transpose(1, 2).view(-1, 3).cpu().numpy()
+    predicted_colors = np.apply_along_axis(lambda i: color2class[int(i)], 1, predicted) / 255
+    
     # create visualizer
     vis = Visualizer()
     # add to visualizer
-    vis.add_by_pointcloud(pc_original)
-    vis.add_by_pointcloud(pc_ground_truth)
-    vis.add_by_pointcloud(pc_predicted)
+    vis.add_by_features(points, colors, normalize=False)
+    vis.add_by_features(points, class_colors, normalize=False)
+    vis.add_by_features(processed_points, predicted_colors, normalize=False)
     # run
     vis.run()